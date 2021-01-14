ca2de78a0954f2de28534151be11dc78dc8c3da1
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-14 18:57:04 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let args = process.argv.slice(2)
+
+console.log(args);
+
+let a = Number(args[1]);
+let b = Number(args[2]);
+let c= "";
+
+if(args[0] == "s"){
+    c = soma(a,b);
+}else if(args[0] == "m"){
+    c = mult(a, b);
+}else{
+    c = "OPÇÃO INVÁLIDA"
+}
+
+
+function soma(a, b){
+    return a+b;
+}
+
+function mult(a, b){
+    return a*b;
+}
+
+console.log(c);
