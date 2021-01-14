7bba6eaf3637e87df23fe1929b44e51e1bd1ea71
eliasfernandesbr <eliasfernandescoutinho@gmail.com>
2021-01-14 19:22:10 -0300

    Encerrando aula1

---
+++ app.js
@@ -0,0 +1,9 @@
+//MODULOS
+var calculadora = require("./calculadora");
+
+
+
+console.log(calculadora.soma(20, 10));
+console.log(calculadora.mult(20, 10));
+console.log(calculadora.sub(20, 10));
+console.log(calculadora.div(20, 10));
---
+++ calculadora.js
@@ -0,0 +1,21 @@
+module.exports = {
+  soma,
+  mult,
+  sub,
+  div
+
+};
+
+function soma(a, b) {
+  return a + b;
+}
+
+function mult(a, b) {
+  return a * b;
+}
+function sub(a, b) {
+  return a - b;
+}
+function div(a, b) {
+  return a / b;
+}
